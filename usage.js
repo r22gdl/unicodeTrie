d73020b58ee5d05dab4dc01c909216e67a6a96d6
rosori01 <raphaelaosorio@gmail.com>
2017-09-15 02:59:57 -0700

    docs: (trie) added extra code to further illustrate basic usage of unicode Trie

--- usage.js
+++ usage.js
@@ -5,6 +5,7 @@ const myTrie = Trie();
 myTrie.stringToTrie('I');
 myTrie.stringToTrie('<3');
 myTrie.stringToTrie('⏣⅀ARNY⏣');
-console.log(myTrie.isStringInTrie('I'));
-console.log(myTrie.isStringInTrie('<3'));
-console.log(myTrie.isStringInTrie('⏣⅀ARNY⏣'));
+console.log(myTrie.isStringInTrie('I')); // true
+console.log(myTrie.isStringInTrie('<3')); // true
+console.log(myTrie.isStringInTrie('⏣⅀ARNY⏣')); // true
+console.log(myTrie).isStringInTrie('⅀ARNY'); //false
