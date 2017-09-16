e9cece1ac8a3977be45cc723a187bbeaf94ea34e
rosori01 <raphaelaosorio@gmail.com>
2017-09-15 20:18:50 -0700

    refactor: (bytepack) separate trie private variables from logic of bytepack library

--- bytepack.js
+++ bytepack.js
@@ -1,180 +1,134 @@
-/******************************* GLOBALS ************************************/
+/* eslint spaced-comment: 0, no-bitwise: 0, jsx-a11y/href-no-hash: 0 */
 
-// NOTE: BYTES ARE ENTERED LITTLE ENDIAN
-/* BASE64 */
+/************************************ CONSTANTS *************************************************/
+
+// INVARIANT: BYTES ARE LITTLE ENDIAN
 const byteWidth = 6;
-var totalBitsInByteStr = 0; //always divisible by 8 | must be updated appropriately when adding a significant bit to byteStr
-var byteStr = '';
-var numSigBitsInByteStr = 0;
-const BASE64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-const BASE64_CACHE = {};
-BASE64.split('').forEach(function (val, index) { BASE64_CACHE[val] = index; });
+const BASE64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+const BASE64_CACHE = (function setBase64Cache() { // EXPLAIN
+  const cache = {};
+  BASE64.split('').forEach((val, index) => { cache[val] = index; });
+  return cache;
+}());
 const bytesPerSymbol = 5;
 
 
-/******************************* METHODS ************************************/
+/************************************ METHODS *************************************************/
 
-// RETURN: the value represented by a character in our base64 schema
-// TESTED: TRUE
-function ord(char) {
-  return BASE64_CACHE[char];
-}
+//Note: For all methods below, base64 characters can be thought of as a sequence of 6 bits,
+// each indexed as 0 thru 5
 
-// RETURN: character that represents given decimal value in our base64 schema
-// TESTED: TRUE
-function char(val) {
+// returns the value represented by a character in our base64 schema
+const ord = function _ord(someChar) {
+  return BASE64_CACHE[someChar];
+};
+
+// returns character that represents given decimal value in our base64 schema
+const char = function _char(val) {
   return BASE64[val];
-}
+};
 
-// RETURN: given the number of bits to add, func returns number of extra bytes that must be added
-// to byte string to fit said bits
-// TESTED: TRUE
-function getNumBytes(numBitsToAdd) {
-  // emptyBits i.e. insignificant bits to be filled
-  const emptyBits = (totalBitsInByteStr === 0) ? byteWidth : totalBitsInByteStr%byteWidth;
-  if (emptyBits >= numBitsToAdd){
-    return 0;
-  } // else
-  const numBytes = Math.floor((numBitsToAdd - emptyBits)/byteWidth) + ((numBitsToAdd - emptyBits)%byteWidth ? 1 : 0);
-  return numBytes;
-}
-
-// PARAMS: index ... the index of the desired bit
-//         c ... some character in our custom BASE64
-// RETURN: the bit at the provided index of the character provided
-// Note: index of the first bit in char 'c' is 0
-// TESTED: FALSE
-function getBitAt(index, c) {
+// returns the bit located at the given index of the character provided
+const getBitAt = function _getBitAt(index, c) {
   const mask = 1 << (index);
   if (typeof c === 'number') {
     return ((c & mask) === 0) ? 0 : 1;
-  } // else
+  }
   return ((ord(c) & mask) === 0) ? 0 : 1;
-}
+};
 
-// PARAMS: str ... string
-// RETURN: the last character in a given string XOR 0 if str is empty
-// TESTED: FALSE
-function getLastChar(str) {
+// returns the last character in a given string XOR 0 if str is empty
+const getLastChar = function _getLastChar(str) {
   if (str === '' && str.length === 0) {
-    return 0;
-  } // else
+    return 0; // EXPLAIN
+  }
   return str.charAt(str.length - 1);
-}
+};
 
-function setLastChar(str, c) {
+const setLastChar = function _setLastChar(someStr, c) {
+  let str = someStr;
   if (str.length === 0 && str === '') {
-    // console.log('str.length === 0');
-    // console.log('c: ', c);
     return str + c;
   }
-  // console.log('str.length: ', str.length);
   str = str.slice(0, str.length - 1);
-  str = str + c;
+  str += c;
   return str;
-}
+};
 
-// PARAMS: c ... some character in our custom BASE64
-// RETURN: an array of bits representing the character provided
-// TESTED: FALSE
-function getBitArray(c) {
-  const emptyBits = (totalBitsInByteStr === 0) ? byteWidth : totalBitsInByteStr%byteWidth;
-  const sigBits = byteWidth - emptyBits;
+// c  - some character in our custom BASE64
+// returns an array of bits representing the character provided
+const getBitArray = function _getBitArray(c) {
   const bitArr = [];
   let counter = 0;
-  while(counter < byteWidth) {
+  while (counter < byteWidth) {
     bitArr.push(getBitAt(counter, c));
     counter += 1;
   }
   return bitArr.reverse();
-}
+};
 
-// PARAMS: index - the index of the bit to be modified
-//         c - some character in our custom BASE64 to be modified,
-//         or some number MAX: 32 bit signed integer
-// RETURN: modified value, NOT NECESSARILY a character belonging to our custom BASE64
-// Note: index of the first bit in char 'c' is 0
-// TESTED: FALSE
-function setBitAt(bit, index, c) {
-  // console.log('setBitAt()');
-  // console.log('bit: ', bit);
-  // console.log('index: ', index);
-  // console.log('c: ', c);
+// c - some character in our custom BASE64 to be modified or some number MAX: 32 bit signed integer
+// returns a modified value, NOT NECESSARILY a character belonging to our custom BASE64
+const setBitAt = function _setBitAt(bit, index, c) {
   let mask;
   if (bit === 0) {
     mask = ~(1 << (index));
     if (typeof c === 'number') {
       return (c & mask);
-    } // else
-    return(ord(c) & mask);
-  } // else bit === 1
+    }
+    return (ord(c) & mask);
+  } // bit === 1
   mask = 1 << (index);
   if (typeof c === 'number') {
     return (c | mask);
-  } // else
+  }
   return (ord(c) | mask);
-}
-  // To put in setBitAt()
-  // if(c === null ||  c === undefined){
-  //   throw 'setBitAt given null or undefined char'
-  // }
+};
 
-// PARAMS:
-// RETURN:
-// TESTED: FALSE
-function getOffsetOfLastBit() {
-  return totalBitsInByteStr%byteWidth;
-  // const offset = totalBitsInByteStr%byteWidth;
-  // if (offset === 0) {
-  //   return 0;
-  // } // else
-  // // return byteWidth - offset;
-  // return byteWidth - offset;
-}
 
-function getIndexToAddBit(offset) {
-  let index = offset + 1;
-  return byteWidth - index; // litte Endian
-}
+const getOffsetOfLastBit = function _getOffsetOfLastBit(numBitsAddedToBase64String) {
+  return numBitsAddedToBase64String % byteWidth;
+};
 
-// PARAMS:
-// RETURN:
-// TESTED: FALSE
-function addBitToByteString(bit) {
-  // console.log('addBitToByteString: ', bit);
-  const emptyBits = (totalBitsInByteStr === 0) ? byteWidth : totalBitsInByteStr%byteWidth;
-  let offsetLastBit = getOffsetOfLastBit();
-  // console.log('offsetLastBit: ', offsetLastBit);
-  const index  = getIndexToAddBit(offsetLastBit);
-  // console.log('indexToAddBit: ', index);
-  if(offsetLastBit === 0) { // add empty byte
-    // console.log('adding empty byte');
-    byteStr += '_'; // REPLACE WITH: addNewByte(byteStr, '_');
-    // console.log('byteStr: ', byteStr);
+// EXPLAIN
+const getIndexToAddBit = function _getIndexToAddBit(offset) {
+  const index = offset + 1;
+  return byteWidth - index; // litte endian
+};
+
+const addBitToBase64String = function _addBitToBase64String(bit, index, someBitArr, numBits, someBase64String) {
+  const offsetLastBit = getOffsetOfLastBit(numBits);
+  const indexToAddBit = getIndexToAddBit(offsetLastBit); // EXPLAIN
+  // add 'empty' byte
+  let base64String = someBase64String;
+  if (offsetLastBit === 0) {
+    base64String += '_';
   }
-  // console.log ('about to set lastChar');
-  // console.log('getLastChar(byteStr): ', getLastChar(byteStr));
-  const lastChar = char(setBitAt(bit, index, getLastChar(byteStr)));
-  byteStr = setLastChar(byteStr, lastChar);
-  // console.log('byteStr: ', byteStr);
-  // console.log('added lastChar above');
-  totalBitsInByteStr += 1;
-}
+  const updatedLastChar = char(setBitAt(bit, indexToAddBit, getLastChar(base64String)));
+  base64String = setLastChar(base64String, updatedLastChar);
+  return base64String;
+};
 
-// ERROR: throws error when called with undefined or null argument
-// PARAMS: bitArr - can't be undefined, cant be null, may be empty array
-// TESTED: UNOFFICIALLY
-function bitArrayToByteString(bitArr) {
-  if(bitArr === null || bitArr === undefined) {
+// bitArr - cannot be undefined, cant be null, may be empty array
+const bitArrayToBase64String = function _bitArrayToBase64String(bitArr) {
+  // count the number of bits that have been added to the base64String
+  let numBitsAddedToBase64String = 0;
+  let base64String = '';
+  if (bitArr === null || bitArr === undefined) {
     // do nothing;
-    throw 'bitArrayToByteString() called w/ undefined || null parameter'
+    throw 'bitArrayToBase64String() called w/ undefined || null parameter';
   } else {
-    bitArr.forEach(addBitToByteString);
+    bitArr.forEach((bit, index, arr) => {
+      base64String =
+        addBitToBase64String(bit, index, arr, numBitsAddedToBase64String, base64String);
+      numBitsAddedToBase64String += 1;
+    });
   }
-}
+  return base64String;
+};
 
 // last byte reserved for 'denotesWord' boolean flag
-var getPadding = function _getPadding(byteLength, denotesWord) {
+const getPadding = function _getPadding(byteLength, denotesWord) {
   const padding = [];
   const fullByte = 0xFF;
   const wordFlag = denotesWord ? 0x01 : 0x00;
@@ -185,7 +139,7 @@ var getPadding = function _getPadding(byteLength, denotesWord) {
   return padding;
 };
 
-var unicodeToByteArray = function _unicodeToByteArray(str) {
+const unicodeSymbolToExpandedByteArray = function _unicodeSymbolToExpandedByteArray(str) {
   let byteArray = [];
   let denotesWord = false;
   for (let i = 0; i < str.length; i += 1) {
@@ -208,7 +162,7 @@ var unicodeToByteArray = function _unicodeToByteArray(str) {
   return byteArray;
 };
 
-var byteArrayToUnicode = function _byteArrayToUnicode(byteArray) {
+const expandedByteArrayToUnicodeSymbol = function _expandedByteArrayToUnicodeSymbol(byteArray) {
   let str = '';
   for (let i = 0; i < byteArray.length; i += 1) {
     if (((i + 1) % bytesPerSymbol) === 0) {
@@ -229,47 +183,8 @@ var byteArrayToUnicode = function _byteArrayToUnicode(byteArray) {
   return decodeURIComponent(str);
 };
 
-/******************************* EXPORTS ************************************/
-
-exports.ord = ord;
-exports.char = char;
-exports.addBitToByteString = addBitToByteString;
-exports.BASE64= BASE64;
-exports.BASE64_CACHE = BASE64_CACHE;
-exports.unicodeToByteArray = unicodeToByteArray;
-exports.byteArrayToUnicode = byteArrayToUnicode;
-exports.bitArrayToByteString = bitArrayToByteString;
-
-// for testing
-exports.byteWidth = byteWidth;
-exports.totalBitsInByteStr = totalBitsInByteStr;
-
-/******************************* MAIN ************************************/
-
-var bitArray = [0,0,0,0,0,0,0];
-try {
-bitArrayToByteString(bitArray);
-} catch (e) {
-  console.log(e);
-}
-console.log('bitArray: ', bitArray);
-for (var i = 0; i < byteStr.length; i += 1) {
-  console.log(getBitArray(byteStr[i]));
-}
-console.log('getBitArray(byteStr) above');
-console.log('byteStr: ', byteStr);
-
-// // console.log(`byteArrayToUnicode(unicodeToByteArray('ü')): `, byteArrayToUnicode(unicodeToByteArray('ü')));
-// // console.log(`byteArrayToUnicode(unicodeToByteArray('ß')):`, byteArrayToUnicode(unicodeToByteArray('ß')));
-
-// console.log(`unicodeToByteArray(0xFF): `, unicodeToByteArray(0xFF));
-// console.log(`byteArrayToUnicode(unicodeToByteArray('ß')):`, byteArrayToUnicode(unicodeToByteArray('ß')));
-
-/****************************** SCRAP ************************************/
-
-// TESTED: FALSE
-// function addBitToByteString() {
-  // must be aware of how many bits in byteStr
-  // i.e. byteStr === 0xFGAA which means 8 + 8 + 8 + 8  32 bits total but in this supposed scenario
-  // only 29 of these bits are meaningful the rest are "unfilled"
-// }
+/************************************ EXPORTS *************************************************/
+exports.addBitToBase64String = addBitToBase64String;
+exports.unicodeSymbolToExpandedByteArray = unicodeSymbolToExpandedByteArray;
+exports.expandedByteArrayToUnicodeSymbol = expandedByteArrayToUnicodeSymbol;
+exports.bitArrayToBase64String = bitArrayToBase64String;
