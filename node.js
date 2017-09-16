25e5130c27f8173ba3964aa1ae9558c018919be8
rosori01 <raphaelaosorio@gmail.com>
2017-09-15 17:44:40 -0700

    style: (node) remove cluttering comments

--- node.js
+++ node.js
@@ -1,16 +1,5 @@
-// Avoiding Object.create() and the new operator
-
-// const node = {
-//   value : '',
-//   isWord: true,
-//   sibling: null, // node
-//   child: null, // node
-// };
-
-// ************************ USING GETTERS/SETTERS ************************
-
-function Node (spec) {
-  let node = spec;
+const Node = function _Node(spec) {
+  const node = spec;
 
   return {
     get value() { return node.value; },
@@ -20,52 +9,6 @@ function Node (spec) {
     set child(child) { node.child = child; },
     set sibling(sibling) { node.sibling = sibling; },
   };
-
-}
+};
 
 exports.Node = Node;
-
-// ****************** NOT USING GETTERS/SETTERS, INSTEAD USING EXPLICIT METHODS ********************
-// const Node = function _Node (spec) {
-//   let node = spec;
-
-//   function getValue() {
-//     return node.value;
-//   }
-//   function getChild() {
-//     return node.child;
-//   }
-//   function getNext() {
-//     return node.next;
-//   }
-//   function setChild(child) {
-//     node.child = child;
-//   }
-//   function setNext(next) {
-//     node.next = next;
-//   }
-
-//   let nodeMethods = {};
-//   nodeMethods.getValue = getValue;
-//   nodeMethods.getChild = getChild;
-//   nodeMethods.getNext = getNext;
-//   nodeMethods.setChild = setChild;
-//   nodeMethods.setNext = setNext;
-
-//   return nodeMethods;
-// };
-
-// exports.Node = Node;
-
-// *********************************Using a starting object and then Object.create() ******************************************* //
-// note: Object.create() depends on functions `function F() {}`, protoype property `F.prototype = o`, `return new F()`
-// const node = {
-//   value : '',
-//   sibling: null, // node
-//   child: null, // node
-// };
-
-
-// const masterNode = Object.create(masterNode);
-
-
