684fc74efa7385c499be20c642e5f80ed0a2df71
rosori01 <raphaelaosorio@gmail.com>
2017-09-16 22:01:08 -0700

    docs: (usage) update usage.js with succint trie example usage

--- usage.js
+++ usage.js
@@ -1,4 +1,7 @@
-const { Trie } = require('./trie.js')
+/* eslint spaced-comment: 0, jsx-a11y/href-no-hash: 0, max-len: 0, no-console: 0*/
+const Bytepack = require('./bytepack.js');
+const { Trie } = require('./trie.js');
+const { Base64SuccinctTrie } = require('./succinctTrie.js');
 
 const myTrie = Trie();
 
@@ -9,3 +12,19 @@ console.log(myTrie.isStringInTrie('I')); // true
 console.log(myTrie.isStringInTrie('<3')); // true
 console.log(myTrie.isStringInTrie('⏣⅀ARNY⏣')); // true
 console.log(myTrie.isStringInTrie('⅀ARNY')); //false
+
+const bitArray = myTrie.trieToBitArray();
+const base64String = Bytepack.bitArrayToBase64String(bitArray);
+const unicodeSymbolString = myTrie.trieToSymbolString();
+const expandedUnicodeSymbolArray = Bytepack.unicodeSymbolsToExpandedByteArray(unicodeSymbolString);
+const positionsOfZeros = myTrie.getPositionsOfZerosInTrie();
+
+// Note: positionsOfZeros is a data structure that, together with the select() method mentioned
+// in Steve Havnov's post, helps implement the Base64SuccinctTrie.isStringInSuccinctTrie() method
+
+const mySuccintTrie = Base64SuccinctTrie(base64String, expandedUnicodeSymbolArray, positionsOfZeros);
+
+console.log(mySuccintTrie.isStringInSuccintTrie('<')); // true
+console.log(mySuccintTrie.isStringInSuccintTrie('⏣⅀ARNY⏣')); // true
+console.log(mySuccintTrie.isStringInSuccintTrie('3')); // false
+console.log(mySuccintTrie.isStringInSuccintTrie('⏣⅀ARNY♛')); // false
