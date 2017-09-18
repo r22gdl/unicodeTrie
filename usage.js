1eb56f122461f5fc2c5643424a6801718ba10c3f
rosori01 <raphaelaosorio@gmail.com>
2017-09-17 22:19:22 -0700

    docs: (usage) replace isStringInSuccinctTrie with isStringAWordInSuccinctTrie

--- usage.js
+++ usage.js
@@ -5,26 +5,29 @@ const { Base64SuccinctTrie } = require('./succinctTrie.js');
 
 const myTrie = Trie();
 
-myTrie.stringToTrie('I');
-myTrie.stringToTrie('<3');
-myTrie.stringToTrie('⏣⅀ARNY⏣');
+myTrie.addStringToTrie('I');
+myTrie.addStringToTrie('<3');
+myTrie.addStringToTrie('⏣⅀ARNY⏣');
+
+
 console.log(myTrie.isStringInTrie('I')); // true
 console.log(myTrie.isStringInTrie('<3')); // true
 console.log(myTrie.isStringInTrie('⏣⅀ARNY⏣')); // true
 console.log(myTrie.isStringInTrie('⅀ARNY')); //false
+console.log('\n');
 
 const bitArray = myTrie.trieToBitArray();
 const base64String = Bytepack.bitArrayToBase64String(bitArray);
 const unicodeSymbolString = myTrie.trieToSymbolString();
 const expandedUnicodeSymbolArray = Bytepack.unicodeSymbolsToExpandedByteArray(unicodeSymbolString);
 const positionsOfZeros = myTrie.getPositionsOfZerosInTrie();
+const nodeNumbersThatDenoteWords = myTrie.getNodesThatDenoteWords();
 
 // Note: positionsOfZeros is a data structure that, together with the select() method mentioned
 // in Steve Havnov's post, helps implement the Base64SuccinctTrie.isStringInSuccinctTrie() method
 
-const mySuccintTrie = Base64SuccinctTrie(base64String, expandedUnicodeSymbolArray, positionsOfZeros);
+const mySuccintTrie = Base64SuccinctTrie(base64String, expandedUnicodeSymbolArray, positionsOfZeros, nodeNumbersThatDenoteWords);
 
-console.log(mySuccintTrie.isStringInSuccintTrie('<')); // true
-console.log(mySuccintTrie.isStringInSuccintTrie('⏣⅀ARNY⏣')); // true
-console.log(mySuccintTrie.isStringInSuccintTrie('3')); // false
-console.log(mySuccintTrie.isStringInSuccintTrie('⏣⅀ARNY♛')); // false
+console.log(mySuccintTrie.isStringAWordInSuccintTrie('<')); // false
+console.log(mySuccintTrie.isStringAWordInSuccintTrie('⏣⅀ARNY⏣')); // true
+console.log(mySuccintTrie.isStringAWordInSuccintTrie('⏣⅀ARNY♛')); // false
